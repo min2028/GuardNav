fe92078d5c764438b977f9e230a5d0baba000a64
Dickson Ngan <dicksonngan101@gmail.com>
2023-06-08 23:31:13 -0700

    move location data into reducer

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,73 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
+import React, { useEffect } from 'react';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentPosition, resetLocation } from '../reducers/LocationReducer';
 import PageContainer from './PageContainer';
+import LoadingSpinner from './LoadingSpinner';
 
 const Map = () => {
+  const dispatch = useDispatch();
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
   });
-  const [currentPosition, setCurrentPosition] = useState(null);
-  let userCircle = null;
+
+  const { currentPosition } = useSelector(
+    (state) => state.location
+  );
+
+  console.log(isLoaded, currentPosition)
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setCurrentPosition({ lat: latitude, lng: longitude });
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-    } else {
-      console.log('Geolocation is not supported by this browser.');
-    }
-  }, []);
+    const getCurrentPosition = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            dispatch(setCurrentPosition({ lat: latitude, lng: longitude }));
+          },
+          (error) => {
+            console.log(error);
+            dispatch(resetLocation());
+          }
+        );
+      } else {
+        console.log('Geolocation is not supported by this browser.');
+        dispatch(resetLocation());
+      }
+    };
 
-  const createCircle = () => {
-    if (currentPosition && window.google) {
-      const { google } = window;
-      const map = new google.maps.Map(document.getElementById('map'), {
-        center: currentPosition,
-        zoom: 16,
-        disableDefaultUI: true,
-      });
-
-      userCircle = new google.maps.Circle({
-        strokeColor: '#4285F4',
-        strokeOpacity: 1,
-        strokeWeight: 2,
-        fillColor: '#4285F4',
-        fillOpacity: 1,
-        map,
-        center: currentPosition,
-        radius: 16,
-      });
-    }
-  };
-
-  useEffect(() => {
-    createCircle();
-  }, [currentPosition]);
+    getCurrentPosition();
+  }, [dispatch]);
 
   return (
     <PageContainer>
       {isLoaded && currentPosition ? (
         <div
-          id="map"
           style={{
             width: '100%',
             height: '100%',
           }}
         >
           {currentPosition && (
-            <Marker position={currentPosition} icon={null} />
+            <GoogleMap
+              mapContainerStyle={{
+                width: '100%',
+                height: '100%',
+              }}
+              zoom={16}
+              center={currentPosition}
+              options={{
+                disableDefaultUI: true,
+                zoomControl: true,
+              }}
+            >
+              <Marker position={currentPosition} />
+            </GoogleMap>
           )}
         </div>
       ) : (
-        <CircularProgress />
+        <LoadingSpinner />
       )}
     </PageContainer>
   );

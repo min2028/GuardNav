68818bcf3a4f323e1533c441f7adc912b9a3c66b
Andres Lee <mush@Andress-MacBook-Pro.local>
2023-06-09 16:25:34 -0700

    feat(cards: MVP for history of cards

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import Map from "./Map";
 import PageContainer from "./PageContainer";
 import LoadingSpinner from "./LoadingSpinner";
 import WeatherInformation from "./WeatherInformation";
+import HistoryCard from "./HistoryCard";
 
-export { Map, PageContainer, LoadingSpinner, WeatherInformation };
+export { Map, PageContainer, LoadingSpinner, WeatherInformation, HistoryCard };

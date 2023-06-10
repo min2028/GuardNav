e5560c5d59d0af255a2793fa56e12297daf57916
Ryosuke Urawa <93410921+Ryo-Ura@users.noreply.github.com>
2023-06-10 14:02:18 -0700

    created NavBar for aboutpage (#15)
    * created NavBar for aboutpage
    
    * updated width

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavBarContainer = styled.div`
+    background-color: #333;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    height: 70px;
+    width: 100%;
+`;
+
+const LeftNav = styled.div`
+  margin-left: auto;
+
+  a {
+    color: #f2f2f2;
+    font-family: "Rubik";
+    text-align: center;
+    text-decoration: none;
+    font-size: 17px;
+    margin: 10px;
+  }
+`;
+
+const MidNav = styled.div`
+  margin: auto;
+  align-self: center;
+
+  a {
+    font-family: "Anek Tamil";
+    letter-spacing: 0.245em;
+    color: #f2f2f2;
+    text-align: center;
+    text-decoration: none;
+    font-size: 17px;
+  }
+`;
+
+const RightNav = styled.div`
+  margin: auto;
+  align-self: center;
+
+  a {
+    color: #f2f2f2;
+    text-align: center;
+    text-decoration: none;
+    font-size: 17px;
+  }
+`;
+
+const NavBar = (props) => {
+  return (
+    <NavBarContainer>
+      <LeftNav>
+        <a className="nav-item" href="../pages/index.js">
+          Home
+        </a>
+        <a className="nav-item" href="../pages/MapPage.js">
+          Map
+        </a>
+        <a className="nav-item" href="../pages/about.js">
+          About
+        </a>
+      </LeftNav>
+      <MidNav>
+        <a className="nav-item" href="../pages/MapPage.js">
+          GuardNav
+        </a>
+      </MidNav>
+      <RightNav>
+        <a className="nav-item" href="../index.html">
+          Help
+        </a>
+      </RightNav>
+    </NavBarContainer>
+  );
+};
+
+export default NavBar;

179fd332f0aa720114cfefebf86cba9db84cd75e
Dickson Ngan <dicksonngan101@gmail.com>
2023-06-14 00:47:13 -0700

    added logo and updated drawer to be persistent

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,33 @@
+import logo from '../logo.png';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin: auto;
+    align-items: center;
+`
+
+const LogoImage = styled.img`
+    max-width: 48px;
+    max-height: auto;
+`
+
+const LogoText = styled.div`
+    font-size: 24px;
+    color: black;
+    margin-left: 8px;
+`
+
+
+const Logo = () => {
+    return (
+        <LogoContainer>
+            <LogoImage src = {logo} alt = "logo" />
+            <LogoText>guardnav</LogoText>
+        </LogoContainer>
+    )
+}
+
+export default Logo;

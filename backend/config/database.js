04877f25b1da6f5024742e33c4ead9a8c7ea89e3
min2028 <ronaldaungkhantmin@gmail.com>
2023-06-19 02:32:06 -0700

    Init express and db

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+uri = process.env.MONGODB_URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((error) => {
+        console.error('Database connection error:', error);
+    });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});

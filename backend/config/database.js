72f518bb553c8b50d812eeadb73fe5b0f060f8d1
min2028 <ronaldaungkhantmin@gmail.com>
2023-06-19 02:32:44 -0700

    Added data model and db setup

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-uri = process.env.MONGODB_URI
-
+uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/guardnav';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to the database');

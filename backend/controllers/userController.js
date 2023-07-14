d7227d2e16e0137f909a656ada5bc506076d0cd2
min2028 <ronaldaungkhantmin@gmail.com>
2023-07-14 15:29:08 -0700

    add history in the user response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,18 @@ const UserModel = require('../models/UserModel');
 
 const userController = {
     getUser: async function (req, res, next) {
-        if (req.user) {
-            res.status(200).send(req.user);
-        } else {
-            res.status(404).send("User not found");
+        try {
+            if (req.user) {
+                const userWithHistory = await UserModel.findById(req.user._id).populate('history');
+                if (!userWithHistory) {
+                    return res.status(404).send("User not found");
+                }
+                res.status(200).json(userWithHistory);
+            } else {
+                res.status(404).send("User not found");
+            }
+        } catch (err) {
+            res.status(500).send(err.message);
         }
     },
     getUserById: async function (req, res, next) {

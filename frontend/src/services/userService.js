caf4f8bc3d911f5445a497e583741b74983cc923
min2028 <93061872+min2028@users.noreply.github.com>
2023-08-04 19:08:33 -0700

    Min3 (#62)
    * Fixed token history
    
    * Added name and number update api
    
    * Update name and number services fe

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -17,7 +17,44 @@ const getUser = async (token) => {
     }
 }
 
+const updateUserName = async (token, id, name) => {
+    try {
+        const response = await axios.patch(
+            `${process.env.REACT_APP_API_URL}/protected/user/${id}/name`,
+            { "name": name },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+        response.data.token = token;
+        return response.data;
+    } catch (err) {
+        console.log(err.message)
+        return err.message();
+    }
+}
+
+const updateUserNumber = async (token, id, number) => {
+    try {
+        const response = await axios.patch(
+            `${process.env.REACT_APP_API_URL}/protected/user/${id}/number`,
+            { "number": number },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+        response.data.token = token;
+        return response.data;
+    } catch (err) {
+        console.log(err.message)
+        return err.message();
+    }
+}
 
 export default {
-    getUser
+    getUser,
+    updateUserName,
+    updateUserNumber
 }
